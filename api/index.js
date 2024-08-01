f79d3ce6fa97301e4f9b865b9497cd79d2703d4e
shiwam singh <96126065+shiwam280@users.noreply.github.com>
2024-08-01 19:54:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://blogit-xy6x.onrender.com", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
